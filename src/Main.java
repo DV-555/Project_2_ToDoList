669154a52a9246a3b2df76243483a0f2c90f65b9
TDumler74 <tatjana.dumler@gmail.com>
2023-05-24 18:25:13 +0200

    feat: add switch

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,21 @@ public class Main {
     System.out.println("4.Удалить задачу");
     System.out.println("5.Выйти" + "\n");
     System.out.print("Выберите действие (1-5): " + "\n");
+
+    int choice = scanner.nextInt();
+
+    switch (choice) {
+      case 1:
+        // создать метод printTasks, каторый выводит список задач из toDoList.txt
+        // =>создать toDoList.txt, res.directory, todo.txt
+        break;
+      case 2://создать метод addTask, чтобы добавить новую задачу
+        break;
+      case 3: // metod saveTaskToFile,сохронить новую задачу в файле
+      case 4: //metod removeTask
+      case 5: //exit
+        //2 commit: add scanner and switch
+
+    }
   }
 }

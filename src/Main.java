cd7fd604b1b0dc547b062896c5968cb0ac6f537a
TDumler74 <tatjana.dumler@gmail.com>
2023-05-24 18:17:59 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Date;
+
 public class Main {
 
   public static void main(String[] args){
-
-
+    Date current = new Date();// для ввывода актуальной даты
+    System.out.println("\n" + "...  Добро пожаловать в Ваш список задач  ...");
+    System.out.println("\n" + "... Сегодня " + current + " ...");
+    System.out.println("\n" + "==============  Список Задач  ===============");
+    System.out.println("\n" + "1.Вывести все задачи ");
+    System.out.println("2.Добавить задачу");
+    System.out.println("3.Сохранить задачи в файл");
+    System.out.println("4.Удалить задачу");
+    System.out.println("5.Выйти" + "\n");
+    System.out.print("Выберите действие (1-5): " + "\n");
   }
 }

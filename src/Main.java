4aac37f08299fc0dd4d6bb5f500c35a641d9b561
Yuliia <angelama2408@gmail.com>
2023-05-25 18:02:47 +0200

    feat: add main

--- src/Main.java
+++ src/Main.java
@@ -1,97 +1,18 @@
-import java.text.SimpleDateFormat;
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.Date;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
-import java.io.*;
-import java.text.SimpleDateFormat;
-import java.util.*;
 
 public class Main {
 
-  private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy HH:mm");
 
-  public static void main(String[] args) {
-
-    Map<Date, String> todoMap = new TreeMap<>();
-
-    Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) throws ParseException, IOException {
     Date current = new Date();// для вывода актуальной даты
-
+    System.out.println("=============================================");
     System.out.println("\n" + "...  Добро пожаловать в Ваш список задач  ...");
     System.out.println("\n" + "... Сегодня " + current + " ...");
     System.out.println("\n" + "==============  Список Задач  ===============");
-    System.out.println("\n" + "1.Вывести все задачи ");
-    System.out.println("2.Добавить задачу");
-    System.out.println("3.Сохранить задачи в файл");
-    System.out.println("4.Удалить задачу");
-    System.out.println("5.Выйти" + "\n");
-    System.out.print("Выберите действие (1-5): " + "\n");
-
-    int choice = scanner.nextInt();
-// =>создать toDoList.txt, res.directory, todo.txt
-    switch (choice) {
-      case 1:
-        printTasks(
-                todoMap);// создать метод printTasks, который выводит список задач из toDoList.txt
-        break;
-      case 2:
-        addTask(todoMap, scanner);//создать метод addTask, чтобы добавить новую задачу
-        break;
-      case 3:
-        saveTodoListToFile(todoMap, "todo.txt");
-        // method saveTaskToFile,сохранить новую задачу в файле
-      case 4: //method removeTask
-      case 5: //exit
-    }
+    System.out.println();
+    Welcome.welcome();
   }
-
-  private static void printTasks(Map<Date, String> todoMap) {
-    if (todoMap.isEmpty()) {
-      System.out.println("Список задач пуст.");
-      return;
-    }
-
-    System.out.println("\n" + "Все задачи:" + "\n");
-
-    for (Map.Entry<Date, String> entry : todoMap.entrySet()) {
-      String task = entry.getValue();
-      Date date = entry.getKey();
-      System.out.println(DATE_FORMAT.format(date) + "  :  " + task);
-    }
-  }
-
-  public static void addTask(Map<Date, String> todoMap, Scanner scanner) {
-    System.out.print("Введите дату и время выполнения задачи (dd.MM.yyyy HH:mm): ");
-    String dateString = scanner.nextLine();
-    Date date;
-    try {
-      date = DATE_FORMAT.parse(dateString);
-    } catch (Exception e) {
-      System.out.println("Неверный формат даты и времени.");
-      return;
-    }
-    System.out.println("\n" + "Введите задачу: ");
-    String task = scanner.nextLine();
-    todoMap.put(date, task);
-    System.out.println("Задача успешно добавлена.");
-  }
-
-  private static void saveTodoListToFile(Map<Date, String> todoMap, String fileName) {
-    try (PrintWriter writer = new PrintWriter(new FileWriter(fileName))) {
-      for (Map.Entry<Date, String> entry : todoMap.entrySet()) {
-        Date date = entry.getKey();
-        String task = entry.getValue();
-        String line = DATE_FORMAT.format(date) + "|" + task;
-        writer.println(line);
-      }
-      System.out.println("Список задач сохранен в файл: " + fileName);
-    } catch (IOException e) {
-      System.out.println("Ошибка записи в файл: " + e.getMessage());
-    }
-  }
-
 }
-
-
 

6993537fbb559292905b21fde89ad985deae5293
TDumler74 <tatjana.dumler@gmail.com>
2023-05-25 18:55:35 +0200

    feat: add try catch

--- src/ToDoTask.java
+++ src/ToDoTask.java
@@ -5,9 +5,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 
@@ -39,50 +37,59 @@ public class ToDoTask {
     this.toDoTask = toDoTask;
   }
 
-  public static void printToDoList() throws IOException {
+  public static void printToDoList() {
     File file = new File("toDo.txt");
-    BufferedReader br = new BufferedReader(new FileReader(file));
-    String line;
-    while ((line = br.readLine()) != null) {
-      System.out.println(line);
+    try {
+      BufferedReader br = new BufferedReader(new FileReader(file));
+      String line;
+      while ((line = br.readLine()) != null) {
+        System.out.println(line);
+      }
+      br.close();
+    } catch (IOException e) {
+      System.out.println("Список пустой" + e.getMessage());
     }
   }
 
-  public static void addToDoList(File filename, BufferedReader bufferedReader)
-      throws IOException {
+  public static void addToDoList(File filename, BufferedReader bufferedReader) {
     File file = new File("toDo.txt");
-    FileWriter FileWriter = new FileWriter(file, true);
-    BufferedWriter bufferedWriter = new BufferedWriter(FileWriter);
-    System.out.println("вы желаете записать новые задачи: ");
-    System.out.println("Дата : ");
-    String newDate = bufferedReader.readLine();
-    System.out.println("Новая задача: ");
-    String newTask = bufferedReader.readLine();
-    bufferedWriter.write(newDate + " : " + newTask);
-    bufferedWriter.write("\n");
-    bufferedWriter.close();
-    printToDoList();
-    System.out.println("Задача успешно добавлена.");
+    try {
+      FileWriter FileWriter = new FileWriter(file, true);
+      BufferedWriter bufferedWriter = new BufferedWriter(FileWriter);
+      System.out.println("Вы желаете записать новые задачи: ");
+      System.out.println("Введите дату :");
+      String newDate = bufferedReader.readLine();
+      System.out.println("Новая задача: ");
+      String newTask = bufferedReader.readLine();
+      bufferedWriter.write(newDate + " : " + newTask);
+      bufferedWriter.write("\n");
+      bufferedWriter.close();
+      printToDoList();
+      System.out.println("Задача успешно добавлена.");
+    } catch (IOException e) {
+      System.out.println("Ошибка при вводе " + e.getMessage());
+    }
   }
 
-  public static void removeTask() throws IOException {
-    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    Set< String> map = new HashSet<>();
-    System.out.println("Вы желаете удалить одну из задач : ");
-    printToDoList();
-    System.out.println("Введите дату :");
-    String data = bufferedReader.readLine();
-    System.out.println("Введите задачу :");
-    String task = bufferedReader.readLine();
-    map.add(data+": "+task);
-    System.out.println(map);
-
-    File file = new File("toDo.txt");
-    String newLine = bufferedReader.readLine();
-    BufferedReader br = new BufferedReader(new FileReader(file));
-
+  public static void removeTask()  {
+    try {
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+      Set<String> map = new HashSet<>();
+      System.out.println("Вы желаете удалить одну из задач : ");
+      printToDoList();
+      System.out.println("Введите дату :");
+      String data = bufferedReader.readLine();
+      System.out.println("Введите задачу :");
+      String task = bufferedReader.readLine();
+      map.add(data + ": " + task);
+      System.out.println(map);
+      File file = new File("toDo.txt");
+      String newLine = bufferedReader.readLine();
+      BufferedReader br = new BufferedReader(new FileReader(file));
+    } catch (IOException e) {
+      System.out.println("Ошибка при вводе " + e.getMessage());
+    }
   }
-
 
 
   public static void toExit() {
--- src/Welcome.java
+++ src/Welcome.java
@@ -2,42 +2,44 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Date;
 
 public class Welcome {
-    public static void welcome() throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        File file = new File("toDo.txt");
 
-
-        boolean running= true;
-        while (running) {
-            System.out.println("=============================================");
-            System.out.println("\n" + "1.Вывести все задачи ");
-            System.out.println("2.Добавить задачу");
-            System.out.println("3.Удалить задачу");
-            System.out.println("4.Выйти" + "\n");
-            System.out.print("Выберите действие (1-4): " + "\n");
-            // что бы сделать выбор нужен switch
-            int choice = Integer.parseInt(bufferedReader.readLine());
-            switch (choice) {
-                case 1:
-                    ToDoTask.printToDoList();
-                    break;
-                case 2:
-                    ToDoTask.addToDoList(file, bufferedReader);
-                    break;
-                case 3:
-                    ToDoTask.removeTask();
-                    break;
-                case 4:
-                    ToDoTask.toExit();
-                    running=false;
-                    break;
-                default:
-                    System.out.println("вы сделали не верный ввод !");
-                    break;
-            }
+  public static void welcome() {
+    try {
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+      File file = new File("toDo.txt");
+      boolean running = true;
+      while (running) {
+        System.out.println("=============================================");
+        System.out.println("\n" + "1.Вывести все задачи ");
+        System.out.println("2.Добавить задачу");
+        System.out.println("3.Удалить задачу");
+        System.out.println("4.Выйти" + "\n");
+        System.out.print("Выберите действие (1-4): " + "\n");
+        // что бы сделать выбор нужен switch
+        int choice = Integer.parseInt(bufferedReader.readLine());
+        switch (choice) {
+          case 1:
+            ToDoTask.printToDoList();
+            break;
+          case 2:
+            ToDoTask.addToDoList(file, bufferedReader);
+            break;
+          case 3:
+            ToDoTask.removeTask();
+            break;
+          case 4:
+            ToDoTask.toExit();
+            running = false;
+            break;
+          default:
+            System.out.println("вы сделали не верный ввод !");
+            break;
         }
+      }
+    } catch (IOException e) {
+      System.out.println("Ошибка при вводе " + e.getMessage());
     }
+  }
 }
